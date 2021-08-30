956b21f1ad690e1530daadc3658136bd33b3abb8
Williams C <wilcontrerasf@gmail.com>
2021-08-30 02:40:39 -0400

    Update acceso service
    * se deja validado que el login llama de manera satisfactoria a la api de siad
    * se deja mientras con el envio de forma data, hay que ver como hacerlo enviado solo un json por ejemplo

--- src/app/services/acceso.service.ts
+++ src/app/services/acceso.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Storage } from '@ionic/storage-angular';
+import { environment } from 'src/environments/environment';
+import { Usuario } from '../interfaces/interfaces';
+import { NavController } from '@ionic/angular';
+
+const URL = environment.url;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccesoService {
+  private usuario: Usuario = {};
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private storage: Storage,
+    private navCtrl: NavController
+  ) { }
+
+  login(user: string, password: string){
+    //const data = { _user: user, _passwd: password };
+    const formData = new FormData();
+    formData.append('_user', user);
+    formData.append('_passwd', password);
+    
+    return new Promise(resolve => {
+      this.http.post(`${ URL }/app_login`, formData).subscribe(
+        async resp => {
+          console.log(resp);
+        }
+      );
+    });
+  }
 }
