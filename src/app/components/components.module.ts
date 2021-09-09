cdbd441ce5eac940d43e0d844bbedd6242a36de9
Williams C <wilcontrerasf@gmail.com>
2021-09-09 18:46:58 -0300

    Update componentes module
    * se agrega las referencias a los componentes de usuario y usuarios, se declaran, exportan e importan
    * se agrega la referencia tambien al ionicmodule

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsuariosComponent
+  ],
+  exports: [
+    UsuariosComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ]
 })
 export class ComponentsModule { }
