f54efec201530cfb33cf02814e88da494f2a0168
Williams C <wilcontrerasf@gmail.com>
2021-09-12 19:44:49 -0300

    Update usuario componente
    * se  agrega las referencias a usuario-service, ui-service, navcontroller
    * se agrega actualizarestado async el cual obtiene el estado devuelto por la funcion de actualizar de usuario-service de acuerdo a esto mostrara un toast de realizado o problemas de ser exitoso se redirecciona al tab1

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/interfaces/interfaces';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { UiServiceService } from 'src/app/services/ui-service.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-usuarios',
@@ -10,11 +13,30 @@ export class UsuariosComponent implements OnInit {
 
   @Input() usuarios: Usuario[] = [];
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private uiService: UiServiceService,
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
     console.log('init componentes usuarios');
     console.log(this.usuarios);
   }
 
+  async actualizaEstado(user: Usuario){
+    console.log('log acutalizaEstado');
+    console.log(user);
+    const valido = await this.usuarioService.actualizaUsuarioEstado(user.id, user.activo);
+
+    if(valido){
+      this.uiService.presentToast('Estado usuario actualizado');
+
+      setTimeout(() => {
+        this.navCtrl.navigateRoot('/main/tabs/tab1', { animated: true });
+      }, 2000);
+    }else{
+      this.uiService.presentToast('Ha ocurrido un problema');
+    }
+  }
 }
