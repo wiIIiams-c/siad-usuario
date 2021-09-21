09074d2aa7ffa5f68ed05205125a11fc02bec3e7
Williams C <wilcontrerasf@gmail.com>
2021-09-21 02:10:29 -0300

    Update login ts
    * se cambia el redirect post validacion de login cambiando del tab1 al tab2

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -40,7 +40,7 @@ export class LoginPage implements OnInit {
     const valido = await this.accesoService.login(this.loginUser.user, this.loginUser.password);
 
     if(valido){
-      this.navCtrl.navigateRoot('/main/tabs/tab1', { animated: true });
+      this.navCtrl.navigateRoot('/main/tabs/tab2', { animated: true });
     }else{
       this.uiService.presentToast('Datos Login Incorrectos');
     }
