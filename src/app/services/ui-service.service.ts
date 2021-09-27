90d9426635cdc662bb61647e7c82a802605999f9
Williams C <wilcontrerasf@gmail.com>
2021-09-27 04:37:05 -0300

    Update ui service ts
    * se agregan las funciones present dismiss para mostrar el lader al iniciar sesion
    * se hace asi ya que al hacerlo directo se presentaba el problema de que post login el loader quedaba statico y no se quitaba
    * se cambia la duracion del toast 1500 -> 2000

--- src/app/services/ui-service.service.ts
+++ src/app/services/ui-service.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import { AlertController, ToastController } from '@ionic/angular';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UiServiceService {
+  isLoading = false;
 
   constructor(
     private alertCtrl: AlertController,
-    private toastCtrl: ToastController
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController
   ) { }
 
   async alertaInformativa(message: string){
@@ -27,9 +29,39 @@ export class UiServiceService {
     const toast = await this.toastCtrl.create({
       message,
       position: 'top',
-      duration: 1500
+      duration: 2000
     });
 
     toast.present();
+  }
+
+  async present(){
+    this.isLoading = true;
+
+    return await this.loadingCtrl.create({
+      message: 'Iniciando Sesion...',
+      spinner: 'lines'
+    }).then(
+      a => {
+        a.present().then(
+          () => {
+            console.log('presented');
+            if(!this.isLoading){
+              a.dismiss().then(
+                () => console.log('abort presenting')
+              );
+            }
+          }
+        );
+      }
+    );
+  }
+
+  async dismiss(){
+    this.isLoading = false;
+
+    return await this.loadingCtrl.dismiss().then(
+      () => console.log('dismissed')
+    );
   }
 }
