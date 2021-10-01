1c927bedecf531f92060756d73032af8c6e6b000
Williams C <wilcontrerasf@gmail.com>
2021-10-01 03:01:45 -0300

    Create storage service
    * se crea el storage service el cual administrara la manipulacion del storage, se hace como servicio ya que es recomendado asi y ademas evitar el mensage de create first

---
+++ src/app/services/storage-service.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageServiceService {
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  async get(key: string): Promise<any> {
+    return await this._storage?.get(key);
+  }
+
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  async clear(){
+    await this._storage.clear();
+  }
+}
